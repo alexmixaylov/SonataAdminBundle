b50f2c372905ee84cabd55e13b7191d46f32abd6
alex <alexmixaylov@gmail.com>
2026-01-16 17:20:40 +0100

    install php rector

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_81)
+
+    ->withRules([
+
+    ])
+
+    ->withSkip([
+        __DIR__ . '/src/Resources/skeleton',
+        __DIR__ . '/vendor/**/Resources/skeleton',
+    ])
+
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
